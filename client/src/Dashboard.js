c8ad701a4bf57361291cdb48cf64683777ae43c4
joshmainac <joshmainac@protonmail.com>
2023-10-25 09:22:21 -0700

    add image to dashboard

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import GameCard from './GameCard';
-// import logo192 from './logo192.png'; // Import the image
+import logo192 from './logo192.png'; // Import the image
 // import logo512 from './logo512.png'; // Import the image
 import './Dashboard.css';
 
@@ -9,6 +9,31 @@ import './Dashboard.css';
 function Dashboard() {
     return (
         <div className="app-container">
+            <GameCard
+                title="Word Search Classic"
+                imageUrl={logo192} // Use the imported image
+
+            />
+            <GameCard
+                title="Word Search Classic"
+            // imageUrl={logo192} // Use the imported image
+
+            />
+            <GameCard
+                title="Word Search Classic"
+            // imageUrl={logo192} // Use the imported image
+
+            />
+            <GameCard
+                title="Word Search Classic"
+            // imageUrl={logo192} // Use the imported image
+
+            />
+            <GameCard
+                title="Word Search Classic"
+            // imageUrl={logo192} // Use the imported image
+
+            />
             <GameCard
                 title="Word Search Classic"
             // imageUrl={logo192} // Use the imported image
